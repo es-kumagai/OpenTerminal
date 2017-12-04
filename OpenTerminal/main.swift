9fe83abd0f82a82e5f7d1e61b32182567da9b452
Tomohiro Kumagai <tomohiro@ez-style.jp>
2017-12-04 15:00:10 +0900

    Initial Commit

---
+++ OpenTerminal/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  OpenTerminal
+//
+//  Created by Tomohiro Kumagai on 12/4/17.
+//  Copyright © 2017 EasyStyle G.K. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
